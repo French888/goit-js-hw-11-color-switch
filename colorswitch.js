b76445adc7f815904c749534c3be18b21e209f65
Oleg Bloschinskyi <carrentkharkov@gmail.com>
2021-08-20 00:34:43 +0300

    20.08.21

---
+++ colorswitch.js
@@ -0,0 +1,43 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ];
+  
+  const refs = {
+    body: document.body,
+    startBtn: document.querySelector('[data-action="start"]'),
+    stopBtn: document.querySelector('[data-action="stop"]')
+  }
+
+  refs.startBtn.addEventListener('click', onStartClick);
+  refs.stopBtn.addEventListener('click', onStopClick);
+
+  let intervalId = null;
+
+  function onStartClick(){
+    intervalId = setInterval(()=>{
+// refs.body.style.background = colors[randomIntegerFromInterval(0, colors.length-1)]
+refs.body.style.background = randomColor();
+    }, 1000);
+    refs.startBtn.disabled = true;
+  };
+
+  function onStopClick(){
+   clearInterval(intervalId);
+    refs.startBtn.disabled = false;
+  }
+
+  const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  function randomColor(){
+    return `rgb(
+    ${randomIntegerFromInterval(0, 255)},
+    ${randomIntegerFromInterval(0, 255)},
+    ${randomIntegerFromInterval(0, 255)})`
+  }
